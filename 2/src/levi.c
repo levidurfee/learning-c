e9e6b49fc19d8762db4a9ef9632966be1a34a415
Levi <levi@teklife.info>
2016-03-19 18:47:23 -0400

    adding versions

--- 2/src/levi.c
+++ 2/src/levi.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <levi.h>
 
+#define LEVI_VERSION 1.0.0
+
 int levi(char * name) {
     printf("%s Levi\r\n", name);
 }
--- 2/src/main.c
+++ 2/src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "levi.h"
 
+#define VERSION 0.1.0
+
 int main(int argc, char *argv[]) {
     levi(argv[1]);
     return 0;
