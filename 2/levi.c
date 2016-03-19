d907e9601275e5041955d9d16041e8067e9006dc
Levi <levi@teklife.info>
2016-03-19 18:01:31 -0400

    using a library

---
+++ 2/levi.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int levi(char * name) {
+    printf("%s Levi\r\n", name);
+}
---
+++ 2/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "levi.h"
+
+int main(int argc, char *argv[]) {
+    levi(argv[1]);
+    return 0;
+}
