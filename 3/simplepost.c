720999cc74c6c1e5c69301cae50187b2732dba2e
Levi <levi@teklife.info>
2016-03-20 00:24:07 -0400

    got command line prompts working

--- 3/simplepost.c
+++ 3/simplepost.c
@@ -3,21 +3,61 @@
  * \ V  V /  __/  __/ | | |
  *  \_/\_/ \___|\___|_| |_|
  * Command Line App
- * Version 0.1.0
+ * Version 0.2.0
  */
 #include <stdio.h>
 #include <string.h>
 #include <curl/curl.h>
 
+#define MAX_LEN 80
+
 int main(int argc, char *argv[])
 {
+  char messageTitle[MAX_LEN];
+  char messageBody[MAX_LEN];
+  char messagePassphrase[MAX_LEN];
+  char messageDeleteAfterFirstView[MAX_LEN];
+
+  char postdata[2048];
+
+  printf("What is the title of your message? ");
+  /* Get the input, with size limit. */
+  fgets(messageTitle, MAX_LEN, stdin);
+   /* Remove trailing newline, if there. */
+  if ((strlen(messageTitle)>0) && (messageTitle[strlen (messageTitle) - 1] == '\n')) {
+    messageTitle[strlen (messageTitle) - 1] = '\0';
+  }
+
+  printf("What is your message? ");
+  /* Get the input, with size limit. */
+  fgets(messageBody, MAX_LEN, stdin);
+   /* Remove trailing newline, if there. */
+  if ((strlen(messageBody)>0) && (messageBody[strlen (messageBody) - 1] == '\n')) {
+    messageBody[strlen (messageBody) - 1] = '\0';
+  }
+
+  printf("What is the passphrase? ");
+  /* Get the input, with size limit. */
+  fgets(messagePassphrase, MAX_LEN, stdin);
+   /* Remove trailing newline, if there. */
+  if ((strlen(messagePassphrase)>0) && (messagePassphrase[strlen (messagePassphrase) - 1] == '\n')) {
+    messagePassphrase[strlen (messagePassphrase) - 1] = '\0';
+  }
+
+  printf("Delete after first view (1 yes | 0 no)? ");
+  /* Get the input, with size limit. */
+  fgets(messageDeleteAfterFirstView, MAX_LEN, stdin);
+   /* Remove trailing newline, if there. */
+  if ((strlen(messageDeleteAfterFirstView)>0) && (messageDeleteAfterFirstView[strlen (messageDeleteAfterFirstView) - 1] == '\n')) {
+    messageDeleteAfterFirstView[strlen (messageDeleteAfterFirstView) - 1] = '\0';
+  }
+
+  sprintf(postdata, "messageTitle=%s&messageBody=%s&messagePassphrase=%s&messageDeleteAfterFirstView=%s", messageTitle, messageBody, messagePassphrase, messageDeleteAfterFirstView);
+
   CURL *curl;
   CURLcode res;
 
-  /*static const char *postthis="messageTitle=hello&messageBody=hello&messagePassphrase=test&messageDeleteAfterFirstView=1";*/
-
-  char postdata[2048];
-  sprintf(postdata, "messageTitle=%s&messageBody=%s&messagePassphrase=%s&messageDeleteAfterFirstView=%s", argv[1], argv[2], argv[3], argv[4]);
+  /*sprintf(postdata, "messageTitle=%s&messageBody=%s&messagePassphrase=%s&messageDeleteAfterFirstView=%s", argv[1], argv[2], argv[3], argv[4]);*/
   /*printf("%s\n", postdata);*/
 
   curl = curl_easy_init();
