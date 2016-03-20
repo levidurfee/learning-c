690d34b3a059263caddcc8fa1f69fa36ae5d6881
Levi <levi@teklife.info>
2016-03-19 23:14:29 -0400

    adding for loop

--- 2/src/main.c
+++ 2/src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <signal.h>
 #include "main.h"
 #include "levi.h"
 #include "durfee.h"
@@ -6,12 +7,18 @@
 #define VERSION 0.1.0
 
 int main(int argc, char *argv[]) {
+    int x;
     levi(argv[1]);
 
     point p;
     p.x = 5;
     p.y = 6;
     durfee(&p);
+
+    for (x = 0; x < 10; x++) {
+        printf("%i", x);
+    }
+    printf("\r\n");
 
     return 0;
 }
